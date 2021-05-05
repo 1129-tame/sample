326d78942dd3e33b736e051dcdd1b4fd50bfe3dd
tame <78590091+1129-tame@users.noreply.github.com>
2021-05-05 14:53:18 +0900

    commit_last

--- javascript/practice/7-02_api/script.js
+++ javascript/practice/7-02_api/script.js
@@ -11,6 +11,11 @@ function fail(error) {
 
 navigator.geolocation.getCurrentPosition(success, fail);
 
+// UTCをミリ秒に
+function utcToJSTime(utcTime) {
+    return utcTime * 1000;
+}
+
 // データ取得
 async function ajaxRequest(lat, long) {
     const appId = '9e163a3723576c6555670dd272585e1b';
@@ -20,7 +25,53 @@ async function ajaxRequest(lat, long) {
     const request = await fetch(url);
     const data = await request.json();
     console.log(data); 
-    console.log(url);
-    console.log(lat);
-    console.log(long);
+    // console.log(url);
+    // console.log(lat);
+    // console.log(long);
+
+    // 都市名・国名
+    console.log('都市名：' + data.city.name);
+    console.log('国名：' + data.city.country);
+    document.getElementById('place').textContent =  data.city.name + ',' + data.city.country;
+
+    // 天気予報データ
+    data.list.forEach(function(forecast, index) {
+        const dataTime = new Date(utcToJSTime(forecast.dt));
+        const month = dataTime.getMonth() + 1;
+        const date = dataTime.getDate();
+        const hours = dataTime.getHours();
+        console.log(month);
+        const min = String(dataTime.getMinutes()).padStart(2, '0');
+        const temperature = Math.round(forecast.main.temp);
+        const description = forecast.weather[0].description;
+        const iconPath = `images/${forecast.weather[0].icon}.svg`;
+
+        console.log('日時：' + `${month}/${date} ${hours}:${min}`);
+        console.log('気温：' + temperature);
+        console.log('天気：' + description);
+        console.log('画像パス：' + iconPath);
+
+        if (index === 0) {
+            const currentWeather = `
+            <div class="icon"><img src="${iconPath}"></div>
+            <div class="info">
+                <p>
+                    <span class="description">現在の天気：${description}</span>
+                    <span class="temp">${temperature}</span>℃
+                </p>
+                </div>`;
+                console.log(currentWeather);
+            document.getElementById("weather").insertAdjacentHTML("beforeend", currentWeather);
+        } else {
+            const tableRow = `
+            <tr>
+                <tb class="info">
+                    ${month}/${date} ${hours}:${min}
+                </tb>
+                <td class="icon"><img src="${iconPath}"></td>
+                <td><span class="description">${description}</span></td>
+            </tr>`;
+            document.getElementById("forecast").insertAdjacentHTML("beforeend", tableRow);
+        }
+    });
 }
